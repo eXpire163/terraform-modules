6b67fbe682d30d8af45ad44fae0877116c92337b
Christian Wiedemann <christian.wiedemann@capgemini.com>
2020-07-21 22:52:56 +0200

    add name

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,4 +4,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-00edb93a4d68784e3"  
   instance_type = var.instance_type
+  tags = {
+    Name = "HelloWorld ${var.instance_name}"
+  }
 }
